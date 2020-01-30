c742828e9155e407f3668bf9f63227d23316dd0c
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-01-30 13:52:48 -0300

    Teste sem mock

--- 05-tests/01/service.js
+++ 05-tests/01/service.js
@@ -5,7 +5,14 @@ const URL = `https://swapi.co/api/people`;
 async function obterPessoas(nome) {
   const url = `${URL}/?search=${nome}&format=json`;
   const response = await get(url);
-  return response.data;
+  return response.data.results.map(mapearPessoas);
+}
+
+function mapearPessoas(item) {
+  return {
+    nome: item.name,
+    peso: item.height
+  };
 }
 
 module.exports = {
