8250e4b9d2445d2266c7d1e6d72c8311ba10f1c1
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-01-30 09:26:54 -0300

    Inicio da aula 5

---
+++ 05-tests/01/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Star Wars Tests', () => {
+  it('deve buscar o r2d2 com o formato correto', async () => {
+    const expected = [{ nome: 'R2-D2', peso: '96' }];
+    const nomeBase = 'r2-d2';
+  });
+});
