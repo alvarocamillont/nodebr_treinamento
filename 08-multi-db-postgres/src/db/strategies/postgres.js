f03ef43025743322c55f2203c56e70fc4e9844b4
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-02-11 19:21:22 -0300

    Criado arquivos do postgres

--- 08-multi-db-postgres/src/db/strategies/postgres.js
+++ 08-multi-db-postgres/src/db/strategies/postgres.js
@@ -1,12 +1,63 @@
 const ICrud = require('./interfaces/interfaceCrud');
+const Sequelize = require('sequelize');
 
 class Postgres extends ICrud {
-  constructor(){
+  constructor() {
     super();
+    this._driver = null;
+    this._herois = null;
+    this._connect();
   }
 
-  create(item){
-    console.log('O item foi salvo no Postgres')
+  async isConnected() {
+    try {
+      await this._driver.authenticate();
+      return true;
+    } catch (error) {
+      console.error('falied', error);
+      return false;
+    }
+  }
+
+  _connect() {
+    this._driver = new Sequelize('heroes', 'alvaro', 'senha123', {
+      host: 'localhost',
+      dialect: 'postgres',
+      quoteIdentifiers: false,
+      operatorsAliases: false
+    });
+  }
+
+  defineModel() {
+    this._herois = this._driver.define(
+      'herois',
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          required: true,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        nome: {
+          type: Sequelize.STRING,
+          required: true
+        },
+        poder: {
+          type: Sequelize.STRING,
+          required: true
+        }
+      },
+      {
+        tableName: 'TB_HEROIS',
+        freezeTableName: false,
+        timestamps: false
+      }
+    );
+    await this._herois.sync();
+  }
+
+  create(item) {
+    console.log('O item foi salvo no Postgres');
   }
 }
 
