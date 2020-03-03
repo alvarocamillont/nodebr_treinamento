8e3b2058c832ac466e45d0f5842286828351645e
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-03-03 13:57:27 -0300

    Criada listagem de página dinâmica

--- 13-api-estrutura/src/api.js
+++ 13-api-estrutura/src/api.js
@@ -2,21 +2,20 @@ const Hapi = require('hapi');
 const Context = require('./db/strategies/base/contextStrategy');
 const MongoDBStrategy = require('./db/strategies/mongodb/mongoDbStrategy');
 const HeroisSchema = require('./db/strategies/mongodb/schemas/heroisSchema');
+const HeroRoutes = require('./routes/heroRoutes');
 const app = new Hapi.Server({
   port:5000
 })
+
+function mapRoutes(instance, methods) {
+  return methods.map(method => instance[method]())
+}
 
 async function main(){
   const connection = MongoDBStrategy.connect();
   const context = new Context(new MongoDBStrategy(connection,HeroisSchema));
   app.route([
-    {
-      path:'/herois',
-      method:'GET',
-      handler:(request, head)=>{
-        return context.read()
-      }
-    }
+    ...mapRoutes(new HeroRoutes(context),HeroRoutes.methods())
   ])
 
   await app.start()
