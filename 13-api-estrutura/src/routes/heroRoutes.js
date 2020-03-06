5786edfdd47e70cdcefea1f7c993ef61ea23df25
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-03-06 18:22:52 -0300

    Método Get

--- 13-api-estrutura/src/routes/heroRoutes.js
+++ 13-api-estrutura/src/routes/heroRoutes.js
@@ -11,7 +11,17 @@ class HeroRoutes extends BaseRoute{
       path:'/herois',
       method:'GET',
       handler:(request, head)=>{
-        return this.db.read();
+        try {
+          const {skip,limit,nome} = request.query
+          const query = nome ? {nome}: {}
+          if (skip && isNaN(skip)) throw Error('Skip deve ser um número')
+          if (limit && isNaN(limit)) throw Error('Limit deve ser um número')
+
+          return this.db.read(query,parseInt(skip),parseInt(limit));
+        } catch (error) {
+          console.log('Deu Ruim', error)
+          return 'Erro interno no servidor'
+        }
       }
     }
   }
--- 13-api-estrutura/src/tests/apiHeros.test.js
+++ 13-api-estrutura/src/tests/apiHeros.test.js
@@ -19,4 +19,41 @@ describe('Suite de testes da API Heroes',()=>{
     assert.deepEqual(statusCode, 200);
     assert.ok(Array.isArray(dados));
   })
+
+  it('lista/herois - deve retornar somente 3 registros', async ()=>{
+    const TAMANHO_LIMITE = 3
+    const result = await app.inject({
+      method:'GET',
+      url:`/herois?skip=0&limit=${TAMANHO_LIMITE}`
+    })
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode,200);
+    assert.ok(dados.length === TAMANHO_LIMITE )
+  })
+
+  it('lista/herois - deve retornar somente 3 registros', async ()=>{
+    const TAMANHO_LIMITE = "AEEE"
+    const result = await app.inject({
+      method:'GET',
+      url:`/herois?skip=0&limit=${TAMANHO_LIMITE}`
+    })
+
+
+    assert.deepEqual(result.payload,'Erro interno no servidor');
+  })
+
+  it('lista/herois - deve filtrar um item', async ()=>{
+    const TAMANHO_LIMITE = 10000
+    const NAME = 'Flash'
+    const result = await app.inject({
+      method:'GET',
+      url:`/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NAME}`
+    })
+
+    const dados = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.deepEqual(statusCode,200);
+    assert.ok(dados[0].nome === NAME);
+  })
 })
