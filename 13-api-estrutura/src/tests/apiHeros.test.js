412d6e8963bf56cb2f94662ed7fa90919694270d
Alvaro Camillo Neto <alvaro.camillo@totvs.com.br>
2020-03-06 19:26:20 -0300

    Rota de create

--- 13-api-estrutura/src/tests/apiHeros.test.js
+++ 13-api-estrutura/src/tests/apiHeros.test.js
@@ -2,14 +2,18 @@ const assert = require('assert');
 const api = require('../api');
 
 let app = {};
-describe('Suite de testes da API Heroes',()=>{
-  before(async ()=>{
+const MOCK_HEROI_CADASTRAR = {
+  nome: 'Chapolin Colorado',
+  poder: 'Marreta Bionica'
+};
+describe('Suite de testes da API Heroes', () => {
+  before(async () => {
     app = await api;
-  })
+  });
 
-  it('listar /herois', async ()=>{
+  it('listar /herois', async () => {
     const result = await app.inject({
-      method:'GET',
+      method: 'GET',
       url: '/herois'
     });
 
@@ -18,42 +22,55 @@ describe('Suite de testes da API Heroes',()=>{
 
     assert.deepEqual(statusCode, 200);
     assert.ok(Array.isArray(dados));
-  })
+  });
 
-  it('lista/herois - deve retornar somente 3 registros', async ()=>{
-    const TAMANHO_LIMITE = 3
+  it('lista/herois - deve retornar somente 3 registros', async () => {
+    const TAMANHO_LIMITE = 3;
     const result = await app.inject({
-      method:'GET',
-      url:`/herois?skip=0&limit=${TAMANHO_LIMITE}`
-    })
+      method: 'GET',
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+    });
     const dados = JSON.parse(result.payload);
     const statusCode = result.statusCode;
-    assert.deepEqual(statusCode,200);
-    assert.ok(dados.length === TAMANHO_LIMITE )
-  })
+    assert.deepEqual(statusCode, 200);
+    assert.ok(dados.length === TAMANHO_LIMITE);
+  });
 
-  it('lista/herois - deve validar os parametros', async ()=>{
-    const TAMANHO_LIMITE = "AEEE"
+  it('lista/herois - deve validar os parametros', async () => {
+    const TAMANHO_LIMITE = 'AEEE';
     const result = await app.inject({
-      method:'GET',
-      url:`/herois?skip=0&limit=${TAMANHO_LIMITE}`
-    })
+      method: 'GET',
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+    });
 
     const statusCode = result.statusCode;
-    assert.deepEqual(statusCode,400);
-  })
+    assert.deepEqual(statusCode, 400);
+  });
 
-  it('lista/herois - deve filtrar um item', async ()=>{
-    const TAMANHO_LIMITE = 10000
-    const NAME = 'Flash'
+  it('lista GET - /herois - deve filtrar um item', async () => {
+    const TAMANHO_LIMITE = 10000;
+    const NAME = 'Flash';
     const result = await app.inject({
-      method:'GET',
-      url:`/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NAME}`
-    })
+      method: 'GET',
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NAME}`
+    });
 
     const dados = JSON.parse(result.payload);
     const statusCode = result.statusCode;
-    assert.deepEqual(statusCode,200);
+    assert.deepEqual(statusCode, 200);
     assert.ok(dados[0].nome === NAME);
-  })
-})
+  });
+
+  it('cadastrar POST - /herois', async () => {
+    const result = await app.inject({
+      method: 'POST',
+      url: `/herois`,
+      payload: MOCK_HEROI_CADASTRAR
+    });
+    const { message, _id } = JSON.parse(result.payload);
+    const statusCode = result.statusCode;
+    assert.ok(statusCode === 200);
+    assert.notStrictEqual(_id, undefined);
+    assert.deepEqual(message, 'Heroi cadastrado com sucesso!');
+  });
+});
